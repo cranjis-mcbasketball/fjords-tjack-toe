ebb40148e55a3cbc7cc00d654327ec3bdd46d195
Mason Borchard <mborchard@k12.com>
2022-11-11 03:51:20 -0500

    feat: 2022-11-11 update to original app

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
-console.log("It's cranjis here serving up some incompetent tic tac toe magic")
-
+console.log("It's cranjis here, dealing some incompetent tic tac toe magic");
 
 // create variables
 var player1;
@@ -11,60 +10,107 @@ var playsArr = [];
 var currentPlayer = player1;
 var scoreP1 = 0;
 var scoreP2 = 0;
-var gamepiece = "X";
-var gameButtons = document.getElementsByClassName("game-button");
+var gamepiece = 'X';
+var gameButtons = document.getElementsByClassName('game-button');
 var currentPlay = 1;
-var endMessage = document.getElementById("end-message");
-var teamOScore = document.getElementById("teamOScore");
-var teamXScore = document.getElementById("teamXScore");
-var turn = document.getElementById("player-turn");
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
-var box3 = document.getElementById("box3");
-var box4 = document.getElementById("box4");
-var box5 = document.getElementById("box5");
-var box6 = document.getElementById("box6");
-var box7 = document.getElementById("box7");
-var box8 = document.getElementById("box8");
-var box9 = document.getElementById("box9");
-
+var endMessage = document.getElementById('end-message');
+var teamFjordScore = document.getElementById('teamFjordScore');
+var teamXScore = document.getElementById('teamXScore');
+var turn = document.getElementById('player-turn');
+var box1 = document.getElementById('box1');
+var box2 = document.getElementById('box2');
+var box3 = document.getElementById('box3');
+var box4 = document.getElementById('box4');
+var box5 = document.getElementById('box5');
+var box6 = document.getElementById('box6');
+var box7 = document.getElementById('box7');
+var box8 = document.getElementById('box8');
+var box9 = document.getElementById('box9');
 
 // check turn
 
-
-
-var whichTurn = function(){
+var whichTurn = function () {
   gamepiece = plays % 2 == 0 ? 'X' : 'O';
-  turn.innerText = 'It\'s team ' + gamepiece + '\'s turn';
+  turn.innerText = "It's team " + gamepiece + "'s turn";
 };
 
 whichTurn();
 
-var checkWinner = function(){
-  if(((playsArr.includes('X1')) && (playsArr.includes('X2')) &&(playsArr.includes('X3'))) || ((playsArr.includes('X4')) && (playsArr.includes('X5')) &&(playsArr.includes('X6'))) || ((playsArr.includes('X7')) && (playsArr.includes('X8')) &&(playsArr.includes('X9'))) || ((playsArr.includes('X1')) && (playsArr.includes('X4')) &&(playsArr.includes('X7'))) || ((playsArr.includes('X2')) && (playsArr.includes('X5')) &&(playsArr.includes('X8'))) || ((playsArr.includes('X3')) && (playsArr.includes('X6')) &&(playsArr.includes('X9'))) || ((playsArr.includes('X1')) && (playsArr.includes('X5')) &&(playsArr.includes('X9'))) || ((playsArr.includes('X3')) && (playsArr.includes('X5')) &&(playsArr.includes('X7')))){
+var checkWinner = function () {
+  if (
+    (playsArr.includes('X1') &&
+      playsArr.includes('X2') &&
+      playsArr.includes('X3')) ||
+    (playsArr.includes('X4') &&
+      playsArr.includes('X5') &&
+      playsArr.includes('X6')) ||
+    (playsArr.includes('X7') &&
+      playsArr.includes('X8') &&
+      playsArr.includes('X9')) ||
+    (playsArr.includes('X1') &&
+      playsArr.includes('X4') &&
+      playsArr.includes('X7')) ||
+    (playsArr.includes('X2') &&
+      playsArr.includes('X5') &&
+      playsArr.includes('X8')) ||
+    (playsArr.includes('X3') &&
+      playsArr.includes('X6') &&
+      playsArr.includes('X9')) ||
+    (playsArr.includes('X1') &&
+      playsArr.includes('X5') &&
+      playsArr.includes('X9')) ||
+    (playsArr.includes('X3') &&
+      playsArr.includes('X5') &&
+      playsArr.includes('X7'))
+  ) {
     console.log('Team X is the winner!');
     endMessage.innerText = 'Team X is the winner!';
     teamXScore.innerText++;
-    return setTimeout(function(){
+    return setTimeout(function () {
       nextRound();
     }, 2000);
-  } else if (((playsArr.includes('O1')) && (playsArr.includes('O2')) &&(playsArr.includes('O3'))) || ((playsArr.includes('O4')) && (playsArr.includes('O5')) &&(playsArr.includes('O6'))) || ((playsArr.includes('O7')) && (playsArr.includes('O8')) &&(playsArr.includes('O9'))) || ((playsArr.includes('O1')) && (playsArr.includes('O4')) &&(playsArr.includes('O7'))) || ((playsArr.includes('O2')) && (playsArr.includes('O5')) &&(playsArr.includes('O8'))) || ((playsArr.includes('O3')) && (playsArr.includes('O6')) &&(playsArr.includes('O9'))) || ((playsArr.includes('O1')) && (playsArr.includes('O5')) &&(playsArr.includes('O9'))) || ((playsArr.includes('O3')) && (playsArr.includes('O5')) &&(playsArr.includes('O7')))){
+  } else if (
+    (playsArr.includes('O1') &&
+      playsArr.includes('O2') &&
+      playsArr.includes('O3')) ||
+    (playsArr.includes('O4') &&
+      playsArr.includes('O5') &&
+      playsArr.includes('O6')) ||
+    (playsArr.includes('O7') &&
+      playsArr.includes('O8') &&
+      playsArr.includes('O9')) ||
+    (playsArr.includes('O1') &&
+      playsArr.includes('O4') &&
+      playsArr.includes('O7')) ||
+    (playsArr.includes('O2') &&
+      playsArr.includes('O5') &&
+      playsArr.includes('O8')) ||
+    (playsArr.includes('O3') &&
+      playsArr.includes('O6') &&
+      playsArr.includes('O9')) ||
+    (playsArr.includes('O1') &&
+      playsArr.includes('O5') &&
+      playsArr.includes('O9')) ||
+    (playsArr.includes('O3') &&
+      playsArr.includes('O5') &&
+      playsArr.includes('O7'))
+  ) {
     console.log('Team O is the winner!');
     endMessage.innerText = 'Team O is the winner!';
-    teamOScore.innerText++;
-    return setTimeout(function(){
+    teamFjordScore.innerText++;
+    return setTimeout(function () {
       nextRound();
     }, 2000);
-  } else if (plays === 9){
+  } else if (plays === 9) {
     console.log('EVERYBODY is a winner :) [aka tie game]');
     endMessage.innerText = 'EVERYBODY is a winner :) [aka tie game]';
-    return setTimeout(function(){
+    return setTimeout(function () {
       nextRound();
     }, 2000);
   } else {
     return;
   }
-}
+};
 
 // var switchTurn = function(){
 //   if(!checkWinner()){
@@ -75,49 +121,42 @@ var checkWinner = function(){
 //     (checkWinner() === "tie") ? endMessage.innerText = "It's a tie" : endMessage.innerText = gamepiece + ' wins!'
 //   }
 
-
-
-
 // gamepiece = plays % 2 == 0 ? 'X' : 'O';
 
 //
-var switchGamepiece = function(){
-  if (gamepiece === 'X'){
+var switchGamepiece = function () {
+  if (gamepiece === 'X') {
     gamepiece = 'O';
     whichTurn();
     return;
   }
-  if (gamepiece === 'O'){
+  if (gamepiece === 'O') {
     gamepiece = 'X';
     whichTurn();
     return;
   }
-}
+};
 
 // change state of button onclick
 var setPiece = function (i) {
-  document.getElementById('box' + i).addEventListener('click', function(e){
-      e.target.setAttribute('value', gamepiece);
-      plays++;
-      console.log(plays);
-      playsArr.push(gamepiece + i);
-      console.log(playsArr);
-      if(plays === 9){
-        checkWinner();
-      } else {
-        switchGamepiece();
-        checkWinner();
-      }
+  document.getElementById('box' + i).addEventListener('click', function (e) {
+    e.target.setAttribute('value', gamepiece);
+    plays++;
+    console.log(plays);
+    playsArr.push(gamepiece + i);
+    console.log(playsArr);
+    if (plays === 9) {
+      checkWinner();
+    } else {
+      switchGamepiece();
+      checkWinner();
+    }
   });
 };
 for (var i = 1; i < 10; i++) {
   gamepiece = plays % 2 == 0 ? 'X' : 'O';
   setPiece(i);
-
 }
-
-
-
 
 // var resetPiece = function (i) {
 //   document.getElementById('box' + i).addEventListener('click', function(e){
@@ -125,9 +164,6 @@ for (var i = 1; i < 10; i++) {
 //       currentPlay += 1;
 //   });
 // };
-
-
-
 
 // see if anybody won
 
@@ -138,49 +174,43 @@ for (var i = 1; i < 10; i++) {
 //   console.log(box1.value);
 // }
 
-
-
 // function to reset and begin new game
-  // function is referenced in reset button element in html
+// function is referenced in reset button element in html
 
-  var nextRound = function(){
-    plays = 0;
-    playsArr = [];
-    endMessage.innerText = '';
-
-    whichTurn();
-    // document.getElementById('reset').addEventListener('click', function(){
-    for (var i = 1; i < 10; i++){
-      document.getElementById('box' + i).value = '';
-      // target.setAttribute('value', '');
-      }
-
-      // var whichTurn = function(){
-      //   gamepiece = plays % 2 == 0 ? 'X' : 'O';
-      //   turn.innerText = 'It\'s team ' + gamepiece + '\'s turn';
-      // };
-
-    // })
-
-  }
-
-var resetGame = function(){
+var nextRound = function () {
   plays = 0;
   playsArr = [];
-  gamepiece = "X";
   endMessage.innerText = '';
-  teamOScore.innerText = 0;
-  teamXScore.innerText = 0;
+
+  whichTurn();
   // document.getElementById('reset').addEventListener('click', function(){
-  for (var i = 1; i < 10; i++){
+  for (var i = 1; i < 10; i++) {
     document.getElementById('box' + i).value = '';
     // target.setAttribute('value', '');
-    }
+  }
 
+  // var whichTurn = function(){
+  //   gamepiece = plays % 2 == 0 ? 'X' : 'O';
+  //   turn.innerText = 'It\'s team ' + gamepiece + '\'s turn';
+  // };
 
   // })
+};;
 
-}
+var resetGame = function () {
+  plays = 0;
+  playsArr = [];
+  gamepiece = 'X';
+  endMessage.innerText = '';
+  teamFjordScore.innerText = 0;
+  teamXScore.innerText = 0;
+  // document.getElementById('reset').addEventListener('click', function(){
+  for (var i = 1; i < 10; i++) {
+    document.getElementById('box' + i).value = '';
+    // target.setAttribute('value', '');
+  }
+
+  // })
+};
 
 resetGame();
-
